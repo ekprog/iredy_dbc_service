1b919a26ae226d9874f06171918c11711191fc2f
Egor Kozelskij <ekozelskiy@integrotechnologies.ru>
2023-10-14 22:31:01 +0300

    feat(dbc): create challenge ucase

--- layers/domain/app.go
+++ layers/domain/app.go
@@ -5,6 +5,7 @@ const (
 	AccessDenied    string = "access_denied"
 	ValidationError string = "validation_error"
 	NotFound        string = "not_found"
+	AlreadyExists   string = "already_exists"
 
 	ProjectNotFound string = "project_not_found"
 	TaskNotFound    string = "task_not_found"
